e1f5d958f5c2e240401ebba18e6c27346ebb2776
ycseo <ycseo@hancom.com>
2016-02-03 09:56:10 +0900

    util created

---
+++ include/util.h
@@ -0,0 +1,42 @@
+
+#ifndef _UTIL_H_
+#define _UTIL_H_
+
+/*
+** Make sure we can call this stuff from C++.
+*/
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+	/*
+	** Dynamic string object
+	*/
+	typedef struct Str Str;
+
+	struct Str {
+		char *z;        /* Text of the string */
+		int nAlloc;     /* Bytes allocated in z[] */
+		int nUsed;      /* Bytes actually used in z[] */
+	};
+
+	/*
+	** Initialize a Str object
+	*/
+	void strInit(Str *p);
+
+	/*
+	** Free all memory held by a Str object
+	*/
+	void strFree(Str *p);
+
+	/*
+	** Add formatted text to the end of a Str object
+	*/
+	int strPrintf(Str *p, const char *zFormat, ...);
+
+#ifdef __cplusplus
+}  /* End of the 'extern "C"' block */
+#endif
+
+#endif /* _UTIL_H_ */
