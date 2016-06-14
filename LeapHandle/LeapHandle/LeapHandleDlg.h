368588a6f84e56ce36388e18d92c7e00aff8185f
ycseo <ycseo@hancom.com>
2016-06-14 18:23:07 +0900

    motion recognize

---
+++ LeapHandle/LeapHandle/LeapHandleDlg.h
@@ -0,0 +1,33 @@
+
+// HncProbeDlg.h : 헤더 파일
+//
+
+#pragma once
+
+
+// CHncProbeDlg 대화 상자
+class CHncProbeDlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CHncProbeDlg(CWnd* pParent = NULL);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+	enum { IDD = IDD_HNCPROBE_DIALOG };
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+	virtual void OnCancel() {}
+	virtual void OnOK() {}
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
